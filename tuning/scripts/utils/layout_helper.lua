9946c0891b82f927069d893435c2fe191adb201b
Moses Malone <me@bipvanwinkle.com>
2024-11-11 00:38:14 -0700

    code cleanup

--- tuning/scripts/utils/layout_helper.lua
+++ tuning/scripts/utils/layout_helper.lua
@@ -1,11 +1,12 @@
 local function generateSlotPositions(rows, cols, x_offset, y_offset, x_gap, y_gap, Vector3)
-	local slotpos = {}
+	local slot_positions = {}
 	for y = 0, rows - 1 do
 		for x = 0, cols - 1 do
-			table.insert(slotpos, Vector3(x * x_gap + x_offset, y * y_gap + y_offset, 0))
+			local slot_postion = Vector3(x * x_gap + x_offset, y * y_gap + y_offset, 0)
+			table.insert(slot_positions, slot_postion)
 		end
 	end
-	return slotpos
+	return slot_positions
 end
 
 return {
